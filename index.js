55fac58b345f2b48626dfe2dbaeddb239727c0a6
Jedidiah Musa <156676740+JedidiahMusa@users.noreply.github.com>
2024-09-19 20:02:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+heart = document.querySelector('like-js');
+red_heart = document.querySelector('like1-js');
+
+document.addEventListener('click', function(){
+  if (heart.style.display = 'flex') {
+    heart.style.display = 'none';
+    if(red_heart.style.display = 'none'){
+      red_heart.style.display = 'flex';
+    }else if(red_heart.style.display = 'flex'){
+      red_heart.style.display = 'none';
+  } 
+  } 
+})
